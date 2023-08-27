91aa3d96f31e7641ed5ff0e2282ab366713c5d85
Alejandra Toro <torogrisalesa@gmail.com>
2023-08-26 22:14:09 -0500

    Pruebas

---
+++ televisores/tv.py
@@ -0,0 +1,79 @@
+class TV:
+    _numTv = 0
+    def __init__(self,marca,estado):
+        self._marca = marca
+        self._estado = estado
+        self._canal = 1
+        self._estado
+        self._precio = 500
+        self._volumen = 1
+        self._control
+        TV._numTv+=1
+    
+    def getMarca(self):
+        return self._marca
+    
+    def setMarca(self,marca):
+        self._marca = marca
+
+    def getCanal(self):
+        return self._canal
+    
+    def setCanal(self,canal):
+        if (canal>=1 and canal<=120 and self._estado==true):
+            self._canal = canal
+
+    def getPrecio(self):
+        return self._precio
+    
+    def setPrecio(self,precio):
+        self._precio = precio
+
+    def getVolumen(self):
+        return self._volumen
+    
+    def setVolumen(self,volumen):
+        if (volumen>=0 and volumen<=7 and self._estado==True):
+            self._volumen = volumen
+    
+    def getControl(self):
+        return self._control
+    
+    def setControl(self,control):
+        self._control = control
+    
+    @classmethod
+    def getNumTV(cls):
+        return cls._numTV 
+    
+    @classmethod
+    def setNumTV(cls,numTV):
+        cls._numTV = numTV
+
+    def turnOn(self):
+        self._estado = True
+        self._canal = 1
+        self._volumen = 1
+
+    def turnOff(self):
+        self._estado = False
+
+    def getEstado(self):
+        return self._estado
+    
+    def canalUp(self):
+        if(self._canal>=1 and self._canal<120 and self._estado==True):
+            self._canal+=1
+    
+    def canalDown(self):
+        if(self._canal>1 and self._canal<=120 and self._estado==True):
+            self._canal-=1
+
+    def volumenDown(self):
+        if(self._volumen>1 and self._canal<=7 and self._estado==True):
+            self._volumen-=1
+
+    def volumenUp(self):
+        if(self._volumen>=1 and self._volumen<7 and self._estado==True):
+            self._volumen+=1
+    
